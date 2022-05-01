8259271cf5492f7a4997ef6cd6f646e49b907a90
GreedNeSS <ruslan199126@gmail.com>
2022-05-01 21:05:40 +0300

    Added endpoints using the Map method

--- Endpoints_MapMethod/Program.cs
+++ Endpoints_MapMethod/Program.cs
@@ -1,6 +1,26 @@
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Map("/", () => "Index");
+app.Map("/users", () => "Users");
+app.Map("/contact", async context =>
+{
+    await context.Response.WriteAsync("Contact");
+});
+
+app.MapGet("/routes", (IEnumerable<EndpointDataSource> endpointSources) =>
+{
+    StringBuilder sb = new StringBuilder();
+    var endpoints = endpointSources.SelectMany(x => x.Endpoints);
+
+    foreach (var endpoint in endpoints)
+    {
+        sb.AppendLine(endpoint.DisplayName);
+    }
+
+    return sb.ToString();
+});
 
 app.Run();
