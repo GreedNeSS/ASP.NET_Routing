b02fcff41f553237e524d9f6aee263fd432cbdae
GreedNeSS <ruslan199126@gmail.com>
2022-05-02 17:14:58 +0300

    Change first route.

--- RouteParameters/Program.cs
+++ RouteParameters/Program.cs
@@ -1,7 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.Map("/users/{id}/{name?}", (string id, string? name) => $"User\nId: {id};\nName: {name??"Undefined"}");
+app.Map("/users/{id}/{name?}", (string id, string? name, HttpContext context) => $"User\nId: {id};" +
+$"\nName: {name??"Undefined"};\nPath: {context.Request.Path}");
 app.Map("/about", async context => await context.Response.WriteAsync($"{context.Request.Path}{context.Request.QueryString}"));
 app.Map("/contact/{**info=/d/}", (string info) => $"{info}");
 app.Map("{controller=Home}/{action=Index}/{id?}",
